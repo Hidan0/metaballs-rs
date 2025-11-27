1e02eee6e52bd99d9d676d6b47ca1602f149c7ac
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2025-11-27 03:05:30 +0100

    refactor(simple): player movement

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -9,23 +9,14 @@ fn main() {
     let mut app = Metaballs::setup();
     let delta_t = 1. / FPS as f32;
 
-    app.update(&[], delta_t);
-
     let mut x_push = 300;
 
     for i in 0..FRAMES_NUM {
-        app.update(
-            &[Action::MovePlayerBy {
-                x: {
-                    if !(-200..=900).contains(&app.player_pos().0) {
-                        x_push *= -1;
-                    }
-                    x_push
-                },
-                y: 0,
-            }],
-            delta_t,
-        );
+        if !(-200..=900).contains(&app.player_pos().0) {
+            x_push *= -1;
+        }
+
+        app.update(&[Action::MovePlayerBy { x: x_push, y: 0 }], delta_t);
 
         let f_name = format!("out/output-{:02}.ppm", i);
         let mut f = File::create(f_name).unwrap();
