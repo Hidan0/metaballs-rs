29f566e9d7fcd1cef6cb456919050f0d37547152
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2025-11-27 18:35:50 +0100

    feat(raylib-demo): logic to move the ball

--- examples/raylib-demo/src/main.rs
+++ examples/raylib-demo/src/main.rs
@@ -1,3 +1,5 @@
+use crate::KeyboardKey::{KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_UP};
+use metaballs_rs::{Action, BG_COLOR};
 use metaballs_rs::{BUFFER_HEIGHT, BUFFER_WIDTH, Metaballs};
 use raylib::prelude::*;
 
@@ -12,12 +14,43 @@ fn main() {
     let image = Image::gen_image_color(BUFFER_WIDTH as i32, BUFFER_HEIGHT as i32, Color::BLACK);
     let mut texture = rl.load_texture_from_image(&thread, &image).unwrap();
 
+    let vel = 250.;
+
+    rl.set_target_fps(60);
+    let mut actions = vec![];
+
     while !rl.window_should_close() {
-        app.update(&[], rl.get_frame_time());
+        actions.clear();
+
+        if rl.is_key_down(KEY_RIGHT) {
+            actions.push(Action::MovePlayerBy {
+                x: vel as i32,
+                y: 0,
+            });
+        }
+        if rl.is_key_down(KEY_LEFT) {
+            actions.push(Action::MovePlayerBy {
+                x: -vel as i32,
+                y: 0,
+            });
+        }
+        if rl.is_key_down(KEY_UP) {
+            actions.push(Action::MovePlayerBy {
+                x: 0,
+                y: -vel as i32,
+            });
+        }
+        if rl.is_key_down(KEY_DOWN) {
+            actions.push(Action::MovePlayerBy {
+                x: 0,
+                y: vel as i32,
+            });
+        }
+
+        app.update(&actions, rl.get_frame_time());
+        let buf = app.buffer();
 
         let mut image_data = vec![0_u8; BUFFER_WIDTH * BUFFER_HEIGHT * 4];
-
-        let buf = app.buffer();
 
         for i in 0..(BUFFER_WIDTH * BUFFER_HEIGHT) {
             let p = buf[i];
@@ -31,6 +64,7 @@ fn main() {
         texture.update_texture(&image_data).unwrap();
 
         rl.draw(&thread, |mut d| {
+            d.clear_background(Color::new(BG_COLOR.r, BG_COLOR.g, BG_COLOR.b, 255));
             d.draw_texture(&texture, 0, 0, Color::WHITE);
         });
     }
